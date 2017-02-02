09bd40882cb4c9089ad80fec1e41d75931c6b071
Dave Hylands <dhylands@gmail.com>
2017-02-01 22:58:21 -0800

    Update to latest MicroPython

--- mpconfigboard.h
+++ mpconfigboard.h
@@ -59,8 +59,8 @@
 #define MICROPY_HW_LED3             (pin_B5)  // blue
 #define MICROPY_HW_LED4             (pin_B0)  // red
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
-#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
-#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
 
 #define MICROPY_HW_UART_REPL        PYB_UART_1
 #define MICROPY_HW_UART_REPL_BAUD   115200
